5dcfea1d30168be17190011eecf6806c24ba5ced
khrom <khrom@DESKTOP-DA2UCIR>
2024-10-29 22:01:26 +0200

    Добавьте файлы проекта.

---
+++ DZ/Program.cs
@@ -0,0 +1,15 @@
+﻿
+    {
+        Console.Write("Введите число A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите число B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Целые числа в диапазоне от A до B:");
+        int k = 0;
+        for (int i = A; i <= B; i++)
+        {
+            Console.WriteLine(i);
+            k++;
+        }
+        Console.WriteLine($"Количество чисел в диапазоне: {k}");
+    }
